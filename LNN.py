686e88ff4b6e5e1a13326e4123b0e4290d4dd726
Pedro Henrique <Ph9084533@gmail.com>
2025-08-31 21:15:48 +0000

    Adicionando o script correto de perguntas. Esutdos de Redes Neourais LNN.

---
+++ LNN.py
@@ -0,0 +1,46 @@
+quiz = [
+    {
+        "pergunta": "Qual banda de rock britânica tem uma música chamada 'Bohemian Rhapsody'?",
+        "resposta": "Queen"
+    },
+    {
+        "pergunta": "Qual famosa cantora pop é conhecida como a 'Rainha do Pop'?",
+        "resposta": "Madonna"
+    },
+    {
+        "pergunta": "Que instrumento musical o famoso músico Jimi Hendrix tocava?",
+        "resposta": "Guitarra"
+    },
+    {
+        "pergunta": "Quem foi o primeiro presidente dos Estados Unidos?",
+        "resposta": "George Washington"
+    },
+    {
+        "pergunta": "Qual foi o nome do navio que afundou em 1912 e inspirou um famoso filme?",
+        "resposta": "Titanic"
+    },
+    {
+        "pergunta": "Em que ano a primeira Copa do Mundo de Futebol foi realizada?",
+        "resposta": "1930"
+    },
+    {
+        "pergunta": "Em qual esporte a brasileira Rayssa Leal, conhecida como 'Fadinha', ganhou medalha olímpica?",
+        "resposta": "Skate"
+    },
+    {
+        "pergunta": "Quantos jogadores um time de basquete tem em quadra ao mesmo tempo?",
+        "resposta": "5"
+    },
+    {
+        "pergunta": "Qual super-herói famoso tem um alter ego chamado Bruce Wayne?",
+        "resposta": "Batman"
+    },
+    {
+        "pergunta": "Qual é o nome do criador do universo de Star Wars?",
+        "resposta": "George Lucas"
+    }
+]
+
+for item in quiz:
+    print(f"Pergunta: {item['pergunta']}")
+    print(f"Resposta: {item['resposta']}\n")
